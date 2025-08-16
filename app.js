85b57d60909ad1e30322ee38460609fa270280aa
Maximiliano Manriquez <rebolledo.maximiliano2708@outlook.cl>
2025-08-15 23:46:25 -0400

    Avances nuevos

--- app.js
+++ app.js
@@ -11,18 +11,27 @@ let listaDeCartas = [];
 const crearCarta = (valor, palo) => {
   const newCard = document.createElement("div");
   newCard.classList.add("card");
+  newCard.classList.add("mx-3");
+  newCard.classList.add("shadow");
 
   const cardHeader = document.createElement("div");
   cardHeader.classList.add("card-header");
+  cardHeader.classList.add("border-0");
   cardHeader.textContent = palo;
 
   const cardBody = document.createElement("div");
   cardBody.classList.add("card-body");
+  cardBody.classList.add("text-center");
   cardBody.textContent = valor;
 
   const cardFooter = document.createElement("div");
   cardFooter.classList.add("card-footer");
   cardFooter.textContent = palo;
+
+  if (palo == "♦" || palo == "♥") {
+    cardHeader.classList.add("heart-diamond");
+    cardFooter.classList.add("heart-diamond");
+  }
 
   newCard.appendChild(cardHeader);
   newCard.appendChild(cardBody);
@@ -33,22 +42,36 @@ const crearCarta = (valor, palo) => {
 
 // Funcion que roba las cartas aleatorias dependiendo de cuantas quiere robar el usuario
 function robarCartas(cantidad) {
+  // Si se ingresa un valor inferior o igual a 0 da error
   if (cantidad < 1) {
     alert("NO PUEDES ROBAR MENOS DE UNA CARTA!");
     return;
   }
+
+  // Limpia la mesa antes de enseñar las cartas nuevas
   container.innerHTML = "";
+
+  // Vacía la lista de cartas cada vez que se roban nuevas cartas
   listaDeCartas = [];
+
   for (let i = 0; i < cantidad; i++) {
+    // cada vez que itera, crea valores aleatorios nuevos
     let valorCarta = Math.floor(Math.random() * valores.length);
     const valorPalo = Math.floor(Math.random() * palos.length);
+
+    // Crea una carta dando como valores los valores aleatorios antes creados
     let carta = crearCarta(valores[valorCarta], palos[valorPalo]);
+
+    // Une la carta creada al div que las almacena
     container.appendChild(carta);
+
+    // Se agregan los valores de las cartas a la lista de cartas para poder sortearlas
     listaDeCartas.push({ valor: valores[valorCarta], palos: palos[valorPalo] });
     console.log(listaDeCartas);
   }
 }
 
+// EventListener que hace la magia
 draw.addEventListener("click", () => {
   let cantidad = Number(cartas.value);
   robarCartas(cantidad);
