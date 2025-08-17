699a9b1e499cd9f0db31258d0e26c65f64a03a1e
Maximiliano Manriquez <rebolledo.maximiliano2708@outlook.cl>
2025-08-17 04:03:32 -0400

    Avances

--- app.js
+++ app.js
@@ -2,10 +2,29 @@ let draw = document.getElementById("robarcartas");
 let ordenar = document.getElementById("ordenarcartas");
 let container = document.getElementById("cardcontainer");
 let cartas = document.getElementById("cantidadcartas");
+let sort = document.getElementById("ordenarcartas");
+let sortContainer = document.getElementById("sortcontainer");
 
-const valores = [2, 3, 4, 5, 6, 7, 8, 9, "J", "Q", "K", "A"];
-const palos = ["♦", "♥", "♠", "♣"];
+//const valores = [2, 3, 4, 5, 6, 7, 8, 9, "J", "Q", "K", "A"];
+const valores = [2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13];
+const palo = ["♦", "♥", "♠", "♣"];
 let listaDeCartas = [];
+
+//Funcion que convierte los valores de la carta a su valor visual
+const obtenerValorVisual = (valor) => {
+  switch (valor) {
+    case 11:
+      return "J";
+    case 12:
+      return "Q";
+    case 13:
+      return "K";
+    case 14:
+      return "A";
+    default:
+      return valor;
+  }
+};
 
 // Funcion que crea una carta
 const crearCarta = (valor, palo) => {
@@ -22,12 +41,13 @@ const crearCarta = (valor, palo) => {
   const cardBody = document.createElement("div");
   cardBody.classList.add("card-body");
   cardBody.classList.add("text-center");
-  cardBody.textContent = valor;
+  cardBody.textContent = obtenerValorVisual(valor);
 
   const cardFooter = document.createElement("div");
   cardFooter.classList.add("card-footer");
   cardFooter.textContent = palo;
 
+  // Condicional para el color del palo
   if (palo == "♦" || palo == "♥") {
     cardHeader.classList.add("heart-diamond");
     cardFooter.classList.add("heart-diamond");
@@ -57,22 +77,72 @@ function robarCartas(cantidad) {
   for (let i = 0; i < cantidad; i++) {
     // cada vez que itera, crea valores aleatorios nuevos
     let valorCarta = Math.floor(Math.random() * valores.length);
-    const valorPalo = Math.floor(Math.random() * palos.length);
+    const valorPalo = Math.floor(Math.random() * palo.length);
 
     // Crea una carta dando como valores los valores aleatorios antes creados
-    let carta = crearCarta(valores[valorCarta], palos[valorPalo]);
+    let carta = crearCarta(valores[valorCarta], palo[valorPalo]);
 
     // Une la carta creada al div que las almacena
     container.appendChild(carta);
 
     // Se agregan los valores de las cartas a la lista de cartas para poder sortearlas
-    listaDeCartas.push({ valor: valores[valorCarta], palos: palos[valorPalo] });
+    listaDeCartas.push({ valor: valores[valorCarta], palo: palo[valorPalo] });
     console.log(listaDeCartas);
   }
 }
 
-// EventListener que hace la magia
+// Funcion que ordena las cartas utilizando el metodo select sort
+function selectSort(cartas) {
+  // Variable que almacena el largo del array de cartas
+  const cantidadCartas = cartas.length;
+
+  // Bucle que contiene el select sort
+  for (let i = 0; i < cantidadCartas - 1; i++) {
+    // Variable que almacena el indice del valor mas pequeño
+    let min = i;
+
+    // Bucle que recorre el array de cartas buscando el numero menor
+    for (let j = i + 1; j < cantidadCartas; j++) {
+      // Si el valor que esta en el indice J es menor que el valor que esta en el indice min, entonces min pasa a tener el valor de j
+      if (cartas[j].valor < cartas[min].valor) min = j;
+    }
+    // Variables auxiliares para el valor de la carta y su palo
+    let tempValor = cartas[i].valor;
+    let tempPalo = cartas[i].palo;
+
+    // Intercambio de valores
+    cartas[i].valor = cartas[min].valor;
+    cartas[i].palo = cartas[min].palo;
+    cartas[min].valor = tempValor;
+    cartas[min].palo = tempPalo;
+  }
+  return cartas;
+}
+
+function ordenarCartas(cartas) {
+  // Si se ingresa un valor inferior o igual a 0 da error
+  if (cartas.length < 1) {
+    alert("PARA ORDENAR LAS CARTAS PRIMERO NECESITAS ROBARLAS!");
+    return;
+  }
+
+  // Limpia la mesa antes de enseñar las cartas nuevas
+  sortContainer.innerHTML = "";
+
+  selectSort(listaDeCartas);
+
+  for (let i = 0; i < cartas.length; i++) {
+    let carta = crearCarta(listaDeCartas[i].valor, listaDeCartas[i].palo);
+    sortContainer.appendChild(carta);
+  }
+}
+
+// EventListeners que hacen la magia
 draw.addEventListener("click", () => {
-  let cantidad = Number(cartas.value);
+  const cantidad = Number(cartas.value);
   robarCartas(cantidad);
+});
+
+sort.addEventListener("click", () => {
+  ordenarCartas(listaDeCartas);
 });
